62ebce90c9a8fb9f57ab72e53d2c5a04c8a6e849
Adrian Majcher <majcherrr87@gmail.com>
2023-05-27 17:42:13 +0200

    Add Navbar, menu hamburger and make responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './assets/theme';
+import Header from './components/Banner/Header';
+
+import './index.css';
 
 function App() {
-  return <div>hej</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Banner>
+          <Header />
+        </Banner>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
+
+const Container = styled.div``;
+const Banner = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`;
---
+++ src/assets/theme.ts
@@ -0,0 +1,6 @@
+const theme = {
+  colors: {
+    green: '#01be96',
+  },
+};
+export default theme;
