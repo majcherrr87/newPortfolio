322bc32baeb2a9e367281bfbded7fbe296cd3330
Adrian Majcher <majcherrr87@gmail.com>
2024-06-18 16:25:01 +0200

    add new favicon

--- src/components/Service/service-data.ts
+++ src/components/Service/service-data.ts
@@ -1,6 +1,7 @@
 import udemy from '../../assets/img/courses/udemy.webp';
 import alx from '../../assets/img/courses/alx.webp';
 import megaK from '../../assets/img/courses/megak.webp';
+import zrozumiecReact from '../../assets/img/courses/zrozumiecReact.webp';
 
 interface DataType {
   id: number;
@@ -32,12 +33,26 @@ const data: DataType[] = [
         disc: `14-miesięczny kurs JavaScriptu. Przygotowujący uczestników do pracy jako junior JavaScript Developer`,
         link: 'https://www.megak.pl/',
         skills: [
-          'JavaScript, TypeScript, OOP, FP, ES6+, asynchroniczność',
-          'Node.js, Express.js, Handlebars, REST, API, TDD',
+          'JavaScript, TypeScript',
+          'Node.js, Express.js, Handlebars',
           'Bazy relacyjne i nierelacyjne, MySQL, MongoDb',
-          'React.js',
+          'React',
           'NestJS, TypeORM,',
+          'Figma',
           'Git, Scrum,',
+        ],
+      },
+      {
+        img: zrozumiecReact,
+        title: 'Zrozumieć React',
+        disc: 'Kurs obejmował ponad 30 godzin materiałów wideo, ponad 150 zadań i 6-godzinny projekt praktyczny.',
+        link: 'https://drive.google.com/file/d/1tEme4gg6umZ8yLG9hYdh9yBq2ytPFc6o/view?usp=drive_link',
+        skills: [
+          'React',
+          'React Router',
+          'Wstęp do Next.js',
+          'CSS Modules, CSS in JS, Tailwind CSS',
+          'Testowanie',
         ],
       },
       {
@@ -53,19 +68,6 @@ const data: DataType[] = [
           'JavaScript',
         ],
       },
-      {
-        img: udemy,
-        title: 'Kursy On-line',
-        disc: ' ',
-        link: 'https://www.udemy.com/certificate/UC-19729b91-bda1-4d71-9487-e64de6c1a84d/',
-        skills: [
-          'Programowanie w JavaScript',
-          'React od podstaw',
-          'Zaawansowane projekty w CSS i JavaScript',
-          'Node.js, Express i MongoDB',
-          'Wprowadzenie do Git i GitHub',
-        ],
-      },
     ],
   },
   {
@@ -73,26 +75,40 @@ const data: DataType[] = [
     lang: 'ENG',
     txtTitleFirst: 'My',
     txtTitleSecond: 'Courses',
-    txtTitleBig: 'Acquired knowledge',
+    txtTitleBig: 'Acquired Knowledge',
     card: [
       {
         img: megaK,
-        title: 'Mega Kurs JavaScript',
-        disc: `14-month JavaScript course. Preparing participants to work as a junior JavaScript Developer`,
+        title: 'Mega JavaScript Course',
+        disc: '14-month JavaScript course. Prepares participants for work as junior JavaScript Developers.',
         link: 'https://www.megak.pl/',
         skills: [
-          'JavaScript, TypeScript, OOP, FP, ES6+, asynchronicity',
-          'Node.js, Express.js, Handlebars, REST, API, TDD',
+          'JavaScript, TypeScript',
+          'Node.js, Express.js, Handlebars',
           'Relational and non-relational databases, MySQL, MongoDb',
-          'React.js',
+          'React',
           'NestJS, TypeORM,',
+          'Figma',
           'Git, Scrum,',
+        ],
+      },
+      {
+        img: zrozumiecReact,
+        title: 'Understanding React',
+        disc: 'The course included over 30 hours of video materials, over 150 tasks, and a 6-hour practical project.',
+        link: 'https://drive.google.com/file/d/1tEme4gg6umZ8yLG9hYdh9yBq2ytPFc6o/view?usp=drive_link',
+        skills: [
+          'React',
+          'React Router',
+          'Introduction to Next.js',
+          'CSS Modules, CSS in JS, Tailwind CSS',
+          'Testing',
         ],
       },
       {
         img: alx,
         title: 'PHP Programmer',
-        disc: `Stationary course - PHP Programmer`,
+        disc: 'In-person course - PHP Programmer',
         link: 'https://www.alx.pl/certyfikat/adrian-majcher/9a73e1ffdf7b415eb22e0ac70384827f/',
         skills: [
           'Introduction to PHP, HTML, CSS, XML',
@@ -100,19 +116,6 @@ const data: DataType[] = [
           'ERD, UML diagrams',
           'Object-Oriented Programming',
           'JavaScript',
-        ],
-      },
-      {
-        img: udemy,
-        title: 'Online courses',
-        disc: ' ',
-        link: 'https://www.udemy.com/certificate/UC-19729b91-bda1-4d71-9487-e64de6c1a84d/',
-        skills: [
-          'Programming in JavaScript',
-          'React from scratch',
-          'Advanced projects in CSS and JavaScript',
-          'Node.js, Express i MongoDB',
-          'Introduction to Git and GitHub',
         ],
       },
     ],
