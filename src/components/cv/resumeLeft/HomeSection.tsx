63a39c5cc036c0e03babc1378f3331fcb8056a17
Adrian Majcher <majcherrr87@gmail.com>
2024-10-23 10:26:58 +0200

    preparing to generate a PDF

--- src/components/cv/resumeLeft/HomeSection.tsx
+++ src/components/cv/resumeLeft/HomeSection.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { BiEnvelope, BiPhone } from 'react-icons/bi';
 import { TbWorldWww } from 'react-icons/tb';
-import { SwitchMode } from './SwitchMode';
-import { Download } from './download';
+// import { SwitchMode } from './SwitchMode';
+// import { Download } from './download';
 import CvLink from '../../../assets/pdf/Adrian_Majcher_CV_ENG.pdf';
-import { ChangeLang } from './ChangeLang';
+// import { ChangeLang } from './ChangeLang';
 
 export function HomeSection() {
   const [userAvatar, setUserAvatar] = useState('');
