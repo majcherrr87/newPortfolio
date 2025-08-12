fd0c9695a29dcb940b33f16355c3d21e75a1d3e9
Adrian Majcher <majcherrr87@gmail.com>
2025-08-12 12:57:30 +0200

    change cv

--- src/components/cv/resumeLeft/HomeSection.tsx
+++ src/components/cv/resumeLeft/HomeSection.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { BiEnvelope, BiPhone } from 'react-icons/bi';
 import { TbWorldWww } from 'react-icons/tb';
-import { SwitchMode } from './SwitchMode';
-import { Download } from './download';
+// import { SwitchMode } from './SwitchMode';
+// import { Download } from './download';
 import CvLink from '../../../assets/pdf/Adrian_Majcher_CV_ENG.pdf';
-import { ChangeLang } from './ChangeLang';
+// import { ChangeLang } from './ChangeLang';
 import { CvContext } from '../context/cvContext';
 
 export function HomeSection() {
